a0d01956528230f55827f246941741902524b37b
Artem Ishchenko <105275043+aishchenko-happy@users.noreply.github.com>
2022-08-03 20:42:34 +0300

    Fix config (#4)
    * fix commit
    
    * fix: update flow
    
    * feat: git hooks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,11 @@
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.18"
+}
+
 rootProject.name = "test-semantic"
 include("app")
+
+gitHooks {
+    commitMsg { conventionalCommits() }
+    createHooks()
+}
