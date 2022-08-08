632a5c36aa4ccb05509789bf8a23bae48fdebc54
Artem Ishchenko <aishchenko@happymoney.com>
2022-08-08 13:35:42 +0300

    fix: update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,2 @@
-plugins {
-    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.0.18"
-}
-
 rootProject.name = "test-semantic"
 include("app")
-
-gitHooks {
-    commitMsg { conventionalCommits() }
-    createHooks()
-}
